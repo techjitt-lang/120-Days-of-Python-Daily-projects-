75cef8edeae8bee7aa220785171863896d8fda01
TechJiT <techjitt@gmail.com>
2025-10-30 10:12:59 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,5 @@
+#Getting User Input
+movie = input('What is your favourite movie: ')
+food = input('What is your favourite food: ')
+place = input('What is your dream travel destination: ')
+print('That is awesome! So you love watching', movie,', Eating',food,'and you wanna visit',place)
